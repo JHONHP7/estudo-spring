3883f936ea039bc2d127edafd0d2a1e8337a3866
jsilva6 <jhonatan.silva@fazenda.rj.gov.br>
2023-09-19 12:27:30 -0300

    Add pagination

--- src/main/java/curso/spring/springboot2/service/AnimeService.java
+++ src/main/java/curso/spring/springboot2/service/AnimeService.java
@@ -7,6 +7,8 @@ import curso.spring.springboot2.requests.AnimePostRequestBody;
 import curso.spring.springboot2.requests.AnimePutRequestBody;
 import curso.spring.springboot2.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,8 +20,8 @@ public class AnimeService {
 
     private final AnimeRepository animeRepository;
 
-    public List<Anime> listAll() {
-        return animeRepository.findAll();
+    public Page<Anime> listAll(Pageable pageable) {
+        return animeRepository.findAll(pageable);
     }
     public List<Anime> findByName(String name) {
         return animeRepository.findByName(name);
