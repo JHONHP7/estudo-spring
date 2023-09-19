47e6411657a1a3e3ee51dc53e846ee92317a4971
jsilva6 <jhonatan.silva@fazenda.rj.gov.br>
2023-09-19 11:45:24 -0300

    Add handler validation

---
+++ documentacao-anotations.md
@@ -0,0 +1,49 @@
+ANOTAIONS SPRING
+@Builder
+A anotação @Builder é usada para gerar automaticamente um padrão de projeto de criação de objetos chamado "Builder Pattern" para uma classe.
+Quando você aplica @Builder a uma classe, o Lombok gera um construtor privado na classe chamado Builder, que permite criar instâncias da classe de forma mais fluente e legível.
+Isso é especialmente útil quando você tem muitos campos em uma classe e deseja criar objetos dessa classe de forma mais concisa e sem a necessidade de chamar um grande construtor com muitos argumentos.
+Exemplo:
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Person {
+    private String name;
+    private int age;
+}
+
+// Uso
+Person person = Person.builder()
+    .name("Alice")
+    .age(30)
+    .build();
+
+@SuperBuilder:
+@SuperBuilder é uma extensão do @Builder.
+Quando aplicado a uma classe, ele gera um construtor na classe filho que chama automaticamente o construtor gerado pelo @Builder da classe pai.
+Isso é útil quando você herda de uma classe que possui um construtor gerado pelo @Builder e deseja criar instâncias da classe filha, herdando os recursos de criação de objetos da classe pai.
+Exemplo:
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@Builder
+public class Person {
+    private String name;
+    private int age;
+}
+
+@SuperBuilder
+public class Employee extends Person {
+    private String employeeId;
+}
+
+// Uso
+Employee employee = Employee.builder()
+    .name("Bob")
+    .age(35)
+    .employeeId("12345")
+    .build();
--- src/main/java/curso/spring/springboot2/handler/RestExceptionHandler.java
+++ src/main/java/curso/spring/springboot2/handler/RestExceptionHandler.java
@@ -1,27 +1,62 @@
 package curso.spring.springboot2.handler;
 
-import curso.spring.springboot2.exception.BadRequestException;
-import curso.spring.springboot2.exception.BadRequestExceptionDetails;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.LocalDateTime;
+import curso.spring.springboot2.exception.BadRequestException;
+import curso.spring.springboot2.exception.BadRequestExceptionDetails;
+import curso.spring.springboot2.exception.ValidationExceptionDetails;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * todos os controladores irão usar essa classe
  */
 @ControllerAdvice
+@Log4j2
 public class RestExceptionHandler {
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre) {
-        return new ResponseEntity<>(BadRequestExceptionDetails.builder()
-                .timestamp(LocalDateTime.now())
-                .status(HttpStatus.BAD_REQUEST.value())
-                .title("Bad request exception, Check the documentation")
-                .details(bre.getMessage())
-                .developerMessage(bre.getClass().getName())
-                .build(), HttpStatus.BAD_REQUEST);
-    }
+	@ExceptionHandler(BadRequestException.class)
+	public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre) {
+		return new ResponseEntity<>(
+				BadRequestExceptionDetails.builder().timestamp(LocalDateTime.now())
+						.status(HttpStatus.BAD_REQUEST.value()).title("Bad request exception, Check the documentation")
+						.details(bre.getMessage()).developerMessage(bre.getClass().getName()).build(),
+				HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
+			MethodArgumentNotValidException exception) {
+		/** 
+		 * PEGANDO AS PARTES COM ERROS
+		 * E AS MENSSAGENS TAMBÉM
+		 */
+		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+		/**
+		 * PEGANDO OS CAMPOS E RETORNANDO DENTRO DE UMA STRING, SE TIVER MASI DE UM VALOR ELE VAI SEPARAR POR VÍRGULA
+		 */
+		String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+		/**
+		 * PEGANDO AS MENSSAGENS
+		 */
+		String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+		return new ResponseEntity<>(
+				ValidationExceptionDetails.builder()
+						.timestamp(LocalDateTime.now())
+						.status(HttpStatus.BAD_REQUEST.value()).title("Bad request exception, Invalid Fields")
+						.details("Check the field(s) error")
+						.developerMessage(exception.getClass().getName())
+						.fields(fields)
+						.fieldsMessage(fieldsMessage)
+						.build(),
+				HttpStatus.BAD_REQUEST);
+		 
+	}
 }
